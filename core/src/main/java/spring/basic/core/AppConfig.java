a7e605ce379ab7f3a4e1d6d438a60739e69045da
Chloe <page.prologue@gmail.com>
2021-05-06 15:54:48 +0900

    app config refactoring

--- core/src/main/java/spring/basic/core/AppConfig.java
+++ core/src/main/java/spring/basic/core/AppConfig.java
@@ -1,6 +1,9 @@
 package spring.basic.core;
 
+import spring.basic.core.discount.DiscountPolicy;
 import spring.basic.core.discount.FixDiscountPolicy;
+import spring.basic.core.discount.RateDiscountPolicy;
+import spring.basic.core.member.MemberRepository;
 import spring.basic.core.member.MemberService;
 import spring.basic.core.member.MemberServiceImpl;
 import spring.basic.core.member.MemoryMemberRepository;
@@ -9,16 +12,24 @@ import spring.basic.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
     /**
      * 실제 구현 객체 생성
      * - 생성자를 통해 주입하는 방식
      */
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
 }
