5e7b85d45ca2d0aa27a1ac08f31ec101d480de4a
Chloe <page.prologue@gamil.com>
2021-06-17 15:51:16 +0900

    proxyMode

--- core/src/main/java/spring/basic/core/web/LogDemoController.java
+++ core/src/main/java/spring/basic/core/web/LogDemoController.java
@@ -1,7 +1,6 @@
 package spring.basic.core.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,13 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURI().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
